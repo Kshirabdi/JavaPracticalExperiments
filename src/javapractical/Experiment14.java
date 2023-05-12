a7844c91db0450618c5b15750f5bb2d6a503caed
Kshirabdi <swainkshirabditanaya@gmail.com>
2023-05-12 23:24:34 +0530

    Experiment 14 added

--- src/javapractical/Experiment14.java
+++ src/javapractical/Experiment14.java
@@ -1,9 +1,17 @@
-package javapractical;
+/*Write a program to create a multilevel package and also creates a reusable class 
+ * to generate Fibonacci series, where the function to generate Fibonacci series is
+ *  given in a different file belonging to the same package.
+ *  written by:kshirabdi
+ *  date:12-05-2023
+ */
 
+package javapractical;
+import javapractical.subpackage.subsubpackage.*;
 public class Experiment14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FiboSeries fs=new FiboSeries();
+		fs.fibonacciSeries();
 
 	}
 
---
+++ src/javapractical/subpackage/subsubpackage/FiboSeries.java
@@ -0,0 +1,23 @@
+package javapractical.subpackage.subsubpackage;
+import java.util.Scanner;
+public class FiboSeries {
+public void fibonacciSeries()
+{
+	Scanner sc= new Scanner(System.in);
+	int n1=0,n2=1;
+	System.out.println("Enter the limit of the series");
+	int limit=sc.nextInt();
+	System.out.print(n1+"   "+n2+"   ");
+	while(true)
+	{
+		int temp=n1+n2;
+		System.out.print(temp+ "   ");
+		n1=n2;
+		n2=temp;
+		if(temp>=limit)
+			break;
+		
+	}
+	
+}
+}
