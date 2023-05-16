3aa9e0525e060cf5cb2627f6017990cc05f0007f
Kshirabdi <swainkshirabditanaya@gmail.com>
2023-05-16 19:55:46 +0530

    cnversuon of decimal to binary program added

---
+++ src/javapractical/Experiment3.java
@@ -0,0 +1,17 @@
+/*To convert a decimal to binary number
+ * written by:kshirabdi
+ * date:16-05-2023
+ */
+package javapractical;
+import java.util.Scanner;
+public class Experiment3 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a decimal number to find its coresponding binary number =");
+		int n=sc.nextInt();
+		System.out.println("binry of "+ n +" = "+Integer.toBinaryString(n));
+		sc.close();
+	}
+
+}
